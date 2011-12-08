1d4bbca1fbaf48357189fed7c6649175dac26ab1
Philipp Schalcher <philipp.schalcher@phischa.ch>
2011-12-08 13:30:17 +0100

    Datenbankconnect hinzugefügt

--- Rezeptverwaltung/src/ch/zhaw/gruppenname/database/database.java
+++ Rezeptverwaltung/src/ch/zhaw/gruppenname/database/database.java
@@ -1,5 +1,45 @@
 package ch.zhaw.gruppenname.database;
 
-public class database {
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
+import com.mysql.*;
+import com.mysql.jdbc.Connection;
+import com.mysql.jdbc.Statement;
+public class database {
+	
+	private String username,password,url;
+	
+	
+	public database()
+	{
+		 username = "";
+		 password = "";
+		 url = "jdbc:mysql://login-18.hoststar.ch/usr_web313_3";
+		 
+		 try {
+			try {
+				Class.forName("com.mysql.jdbc.Driver");
+			} catch (ClassNotFoundException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			// Setup the connection with the DB
+			java.sql.Connection connect = null;
+			connect = DriverManager.getConnection(url,username,password);
+			java.sql.Statement statement = connect.createStatement();
+			
+		 } catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		 
+		 
+		 System.out.println("Erfolg!!!");
+	}
+	
+	public static void main(String[] args) {
+		new database();
+	}
+	
 }
